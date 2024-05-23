17892050a3b9caea9d211e04ff382f2f384fb0f1
Vlad Patoka <patrik.vv2002@gmail.com>
2024-05-23 17:10:11 +0300

    fix tf

--- main.tf
+++ main.tf
@@ -8,15 +8,15 @@ resource "aws_instance" "EmptyInstance" {
   ami           = "ami-0705384c0b33c194c"
   instance_type = "t3.micro"
   key_name      = "EmptyProject"
-  security_groups = [aws_security_group.empty-web-sg3.name]
+  security_groups = [aws_security_group.new-web-sg.name]
 
   tags = {
     Name = "MyEmptyInstance"
   }
 }
 
-resource "aws_security_group" "empty-web-sg3" {
-  name        = "empty-web-sg3"
+resource "aws_security_group" "new-web-sg" {
+  name        = "new-web-sg"
   description = "Allow HTTP traffic"
 
   ingress {
