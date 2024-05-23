41493a79f5531d80091a2c8578ec978331b6bc08
Vlad Patoka <patrik.vv2002@gmail.com>
2024-05-23 16:36:14 +0300

    terraform

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "eu-north-1"
+}
+
+resource "aws_instance" "EmptyInstance" {
+  ami           = "ami-0705384c0b33c194c"
+  instance_type = "t3.micro"
+  key_name      = "my-key-pair"
+
+  tags = {
+    Name = "MyEmptyInstance"
+  }
+
+  security_groups = ["web-sg"]
+}
+
+resource "aws_security_group" "web-sg" {
+  name        = "web-sg"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIAZI2LI75H6R6YJZNA"
+aws_secret_key = "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"
