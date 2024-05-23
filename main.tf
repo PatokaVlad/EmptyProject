b1772e2af6537aa11c4ff65717b8f7de25cd8280
Vlad Patoka <patrik.vv2002@gmail.com>
2024-05-23 17:56:01 +0300

    fix tf

--- main.tf
+++ main.tf
@@ -7,4 +7,29 @@ provider "aws" {
 resource "aws_instance" "EmptyInstance" {
   ami           = "ami-0705384c0b33c194c"
   instance_type = "t3.micro"
+  key_name      = "EmptyProject"
+  security_groups = [aws_security_group.web.name]
+
+  tags = {
+    Name = "MyEmptyInstance"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "new4"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
